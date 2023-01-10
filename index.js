ee3b1e259ba9a4a4bb4911d978c9380b3b51254c
Madison Evans <m.corbinevans@gmail.com>
2023-01-10 09:49:33 -0700

    commit tree f0d86cd15281c6d7713445f29a696e87f76686dd

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+totalBatteries = batteryBatches;
+const reducer = (inputArray, agg = 0) => {
+  inputArray.forEach((value) => {
+    agg += value;
+  });
+  return agg;
+};
